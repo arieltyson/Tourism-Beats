13f6e7ef983ad52e93768e94f77775f6e57ad9cd
Ariel Tyson <ariel30190@hotmail.com>
2024-06-17 18:49:35 -0700

    Initial Commit

---
+++ Tourism Beats/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tourism Beats
+//
+//  Created by Ariel Tyson on 17/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tourism Beats/Tourism_BeatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tourism_BeatsApp.swift
+//  Tourism Beats
+//
+//  Created by Ariel Tyson on 17/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Tourism_BeatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
