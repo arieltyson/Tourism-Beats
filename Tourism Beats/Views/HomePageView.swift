3d00d6fd3b494e350bc3bbc2adf0f0d608122e7b
Ariel Tyson <ariel30190@hotmail.com>
2024-06-17 21:45:55 -0700

    Use SceneKit to create immersive animated background on homepage

--- Tourism Beats/Views/HomePageView.swift
+++ Tourism Beats/Views/HomePageView.swift
@@ -10,18 +10,46 @@ import SwiftUI
 struct HomePageView: View {
     var body: some View {
         NavigationView {
-            VStack {
-                NavigationLink(destination: CitySelectionView()) {
-                    Text("Choose a City")
-                        .font(.largeTitle)
-                        .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
+            ZStack {
+                EarthSceneView()
+                    .edgesIgnoringSafeArea(.all) // Make the scene view full screen
+                VStack(spacing: 20) {
+                    VStack {
+                        Text("Tourism Beats")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .italic()
+                        Text("an immersive tourist experience")
+                            .font(.subheadline)
+                            .foregroundColor(.white)
+                            .italic()
+                    }
+                    .padding(.top, 50)
+                    
+                    Spacer()
+                    
+                    NavigationLink(destination: CitySelectionView()) {
+                        Text("Choose a City")
+                            .font(.title)
+                            .padding()
+                            .background(Color.black.opacity(0.5)) // Transparent background
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.white, lineWidth: 2) // White border for visibility
+                            )
+                    }
+                    
+                    Spacer()
                 }
+                .padding()
             }
-            .navigationTitle("Tourism Beats")
+            .edgesIgnoringSafeArea(.all) // Ensure the entire ZStack ignores the safe area
+            .navigationTitle("") // Empty navigation title for better appearance
         }
+        .navigationViewStyle(StackNavigationViewStyle()) // Ensures proper behavior on iPad
     }
 }
 
