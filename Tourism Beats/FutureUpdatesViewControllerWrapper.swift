674f29e376324815ec533f2f21351afbc1187e15
Ariel Tyson <114557791+arieltyson@users.noreply.github.com>
2025-05-10 12:03:53 -0700

    Remove orphaned file

--- Tourism Beats/FutureUpdatesViewControllerWrapper.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-struct FutureUpdatesViewControllerWrapper: UIViewControllerRepresentable {
-        func makeUIViewController(context: Context) -> FutureUpdatesViewController {
-            return FutureUpdatesViewController()
-        }
-        
-        func updateUIViewController(_ uiViewController: FutureUpdatesViewController, context: Context) {}
-    }
