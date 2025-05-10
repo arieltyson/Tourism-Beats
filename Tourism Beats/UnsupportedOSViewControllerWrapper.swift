56bf597f2d80e9d148159d253d8bf905c4fe0b3c
Ariel Tyson <114557791+arieltyson@users.noreply.github.com>
2025-05-10 12:16:27 -0700

    Remove duplicated files

--- Tourism Beats/UnsupportedOSViewControllerWrapper.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-struct UnsupportedOSViewControllerWrapper: UIViewControllerRepresentable {
-    func makeUIViewController(context: Context) -> UnsupportedOSViewController {
-        return UnsupportedOSViewController()
-    }
-    
-    func updateUIViewController(_ uiViewController: UnsupportedOSViewController, context: Context) {}
-}
