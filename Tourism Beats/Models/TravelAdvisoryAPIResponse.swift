e64a83d19efe8048748c31b9f4473f9582c02f8d
Ariel Tyson <114557791+arieltyson@users.noreply.github.com>
2025-05-10 00:28:52 -0700

    Remove remnants of orphaned Safety Advisory API

--- Tourism Beats/Models/TravelAdvisoryAPIResponse.swift
+++ Tourism Beats/Models/TravelAdvisoryAPIResponse.swift
@@ -1,28 +1,28 @@
-import Foundation
-
-struct TravelAdvisoryAPIResponse: Codable {
-    let data: [String: SafetyDataModel]
-    let apiStatus: APIStatus?
-
-    enum CodingKeys: String, CodingKey {
-        case data
-        case apiStatus = "api_status"
-    }
-}
-
-struct APIStatus: Codable {
-    let request: Request
-    let reply: Reply
-
-    struct Request: Codable {
-        let item: String
-    }
-
-    struct Reply: Codable {
-        let cache: String
-        let code: Int
-        let status: String
-        let note: String
-        let count: Int
-    }
-}
+//import Foundation
+//
+//struct TravelAdvisoryAPIResponse: Codable {
+//    let data: [String: SafetyDataModel]
+//    let apiStatus: APIStatus?
+//
+//    enum CodingKeys: String, CodingKey {
+//        case data
+//        case apiStatus = "api_status"
+//    }
+//}
+//
+//struct APIStatus: Codable {
+//    let request: Request
+//    let reply: Reply
+//
+//    struct Request: Codable {
+//        let item: String
+//    }
+//
+//    struct Reply: Codable {
+//        let cache: String
+//        let code: Int
+//        let status: String
+//        let note: String
+//        let count: Int
+//    }
+//}
