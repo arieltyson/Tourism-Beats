a544e5fe7c16ce60be0a31ba24e29089f7995813
Ariel Tyson <114557791+arieltyson@users.noreply.github.com>
2025-05-18 19:25:40 -0700

    Recreate Wrappers folder as a folder instead of a group

---
+++ Tourism Beats/Wrappers/UIKitGradientBackgroundWrapper.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+import UIKit
+
+struct UIKitGradientBackgroundWrapper: UIViewRepresentable {
+
+    func makeUIView(context: Context) -> UIView {
+        return GradientBackgroundView()
+    }
+
+    func updateUIView(_ uiView: UIView, context: Context) {}
+}
---
+++ Tourism Beats/Wrappers/UnsupportedOSViewControllerWrapper.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct UnsupportedOSViewControllerWrapper: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UnsupportedOSViewController {
+        return UnsupportedOSViewController()
+    }
+
+    func updateUIViewController(
+        _ uiViewController: UnsupportedOSViewController,
+        context: Context
+    ) {}
+}
