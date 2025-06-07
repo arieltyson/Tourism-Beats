134a05ecb53a17ee58d34c375829095b191fa36a
Ariel Tyson <114557791+arieltyson@users.noreply.github.com>
2025-06-07 11:24:15 -0700

    Clean up old files

--- Tourism Beats/Models/TipModel.swift
+++
@@ -1,9 +0,0 @@
-//
-//  TipModel.swift
-//  Tourism Beats
-//
-//  Created by Ariel Tyson on 14/7/24.
-//
-
-import Foundation
-
--- Tourism Beats/SupportingFiles/VisaServiceProtocol.swift
+++
@@ -1,5 +0,0 @@
-import Foundation
-
-protocol VisaServiceProtocol {
-    func checkVisaFreeTravel(fromCountryCode: String, toCountryCode: String) async throws -> Bool
-}
