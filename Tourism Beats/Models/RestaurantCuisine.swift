043099334498115f4c98f3ec2bd0257693916e8d
Ariel Tyson <114557791+arieltyson@users.noreply.github.com>
2026-03-16 19:58:20 -0700

    enhancements

--- Tourism Beats/Models/RestaurantCuisine.swift
+++ Tourism Beats/Models/RestaurantCuisine.swift
@@ -2,36 +2,41 @@ import Foundation
 
 enum RestaurantCuisine: String, CaseIterable, Identifiable, Sendable {
     case american
+    case bakery
+    case brazilian
+    case cafe
+    case caribbean
+    case chinese
+    case dessert
+    case english
+    case ethiopian
+    case french
+    case fusion
+    case german
+    case greek
+    case indian
     case international
     case italian
-    case french
-    case spanish
-    case mediterranean
-    case middleEastern
-    case greek
-    case turkish
-    case lebanese
-    case persian
-    case mexican
-    case peruvian
-    case brazilian
-    case caribbean
     case japanese
-    case chinese
     case korean
-    case thai
-    case vietnamese
-    case indian
-    case ethiopian
-    case seafood
-    case steakhouse
-    case vegetarian
-    case vegan
-    case fusion
-    case cafe
-    case bakery
-    case dessert
+    case lebanese
+    case mediterranean
+    case mexican
+    case middleEastern
     case other
+    case persian
+    case peruvian
+    case portuguese
+    case russian
+    case seafood
+    case snack
+    case spanish
+    case steakhouse
+    case thai
+    case turkish
+    case vegan
+    case vegetarian
+    case vietnamese
 
     var id: String { self.rawValue }
 
@@ -39,66 +44,76 @@ enum RestaurantCuisine: String, CaseIterable, Identifiable, Sendable {
         switch self {
         case .american:
             "American"
+        case .bakery:
+            "Bakery"
+        case .brazilian:
+            "Brazilian"
+        case .cafe:
+            "Cafe"
+        case .caribbean:
+            "Caribbean"
+        case .chinese:
+            "Chinese"
+        case .dessert:
+            "Dessert"
+        case .english:
+            "English"
+        case .ethiopian:
+            "Ethiopian"
+        case .french:
+            "French"
+        case .fusion:
+            "Fusion"
+        case .german:
+            "German"
+        case .greek:
+            "Greek"
+        case .indian:
+            "Indian"
         case .international:
             "International"
         case .italian:
             "Italian"
-        case .french:
-            "French"
-        case .spanish:
-            "Spanish"
-        case .mediterranean:
-            "Mediterranean"
-        case .middleEastern:
-            "Middle Eastern"
-        case .greek:
-            "Greek"
-        case .turkish:
-            "Turkish"
-        case .lebanese:
-            "Lebanese"
-        case .persian:
-            "Persian"
-        case .mexican:
-            "Mexican"
-        case .peruvian:
-            "Peruvian"
-        case .brazilian:
-            "Brazilian"
-        case .caribbean:
-            "Caribbean"
         case .japanese:
             "Japanese"
-        case .chinese:
-            "Chinese"
         case .korean:
             "Korean"
-        case .thai:
-            "Thai"
-        case .vietnamese:
-            "Vietnamese"
-        case .indian:
-            "Indian"
-        case .ethiopian:
-            "Ethiopian"
-        case .seafood:
-            "Seafood"
-        case .steakhouse:
-            "Steakhouse"
-        case .vegetarian:
-            "Vegetarian"
-        case .vegan:
-            "Vegan"
-        case .fusion:
-            "Fusion"
-        case .cafe:
-            "Cafe"
-        case .bakery:
-            "Bakery"
-        case .dessert:
-            "Dessert"
+        case .lebanese:
+            "Lebanese"
+        case .mediterranean:
+            "Mediterranean"
+        case .mexican:
+            "Mexican"
+        case .middleEastern:
+            "Middle Eastern"
         case .other:
             "Other"
+        case .persian:
+            "Persian"
+        case .peruvian:
+            "Peruvian"
+        case .portuguese:
+            "Portuguese"
+        case .russian:
+            "Russian"
+        case .seafood:
+            "Seafood"
+        case .snack:
+            "Snack"
+        case .spanish:
+            "Spanish"
+        case .steakhouse:
+            "Steakhouse"
+        case .thai:
+            "Thai"
+        case .turkish:
+            "Turkish"
+        case .vegan:
+            "Vegan"
+        case .vegetarian:
+            "Vegetarian"
+        case .vietnamese:
+            "Vietnamese"
         }
     }
 }
